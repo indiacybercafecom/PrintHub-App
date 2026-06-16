fbe0eda400af5b771397bd2bb6a37c59a2e1304f
sanjaykumarvaishya <sureshvaishya321@gmail.com>
2026-06-16 09:32:56 +0530

    First Push. app icon and splash screen added.

---
+++ ICC_PrintHub/PrintHub/app/src/main/java/com/indiacybercafe/printhub/SplashActivity.kt
@@ -0,0 +1,35 @@
+package com.indiacybercafe.printhub
+
+import android.annotation.SuppressLint
+import android.content.Intent
+import android.os.Bundle
+import android.os.Handler
+import android.os.Looper
+import androidx.activity.enableEdgeToEdge
+import androidx.appcompat.app.AppCompatActivity
+import androidx.core.view.ViewCompat
+import androidx.core.view.WindowInsetsCompat
+import androidx.core.view.WindowInsetsControllerCompat
+
+@SuppressLint("CustomSplashScreen")
+class SplashActivity : AppCompatActivity() {
+    override fun onCreate(savedInstanceState: Bundle?) {
+        super.onCreate(savedInstanceState)
+        
+        // Enable Edge-to-Edge
+        enableEdgeToEdge()
+        
+        setContentView(R.layout.activity_splash)
+
+        // Hide system bars for immersive mode
+        val windowInsetsController = ViewCompat.getWindowInsetsController(window.decorView) ?: return
+        windowInsetsController.systemBarsBehavior = WindowInsetsControllerCompat.BEHAVIOR_SHOW_TRANSIENT_BARS_BY_SWIPE
+        windowInsetsController.hide(WindowInsetsCompat.Type.systemBars())
+
+        // 2-second delay to transition to MainActivity
+        Handler(Looper.getMainLooper()).postDelayed({
+            startActivity(Intent(this, MainActivity::class.java))
+            finish()
+        }, 2000)
+    }
+}
